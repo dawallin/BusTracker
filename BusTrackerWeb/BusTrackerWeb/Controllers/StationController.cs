d575432beb399008db64c49496b749e3b94fde63
dawallin <wallin@openmind.se>
2014-02-13 23:51:45 +0100

    StationController as first WEB API

---
+++ BusTrackerWeb/BusTrackerWeb/Controllers/StationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BusTrackerDomain;
+using BusTrackerDomain.Entity;
+using BusTrackerDomain.Interface;
+using BusTrackerWeb.SkanetrafikenServiceAgent;
+
+namespace BusTrackerWeb
+{
+    public class StationController : ApiController
+    {
+        static readonly IServiceAgent agent = new ServiceAgent(new WebCallProxy());
+        static readonly BusTracker busTracker = new BusTracker(agent);
+
+        //// GET api/<controller>
+        //public IEnumerable<StationInfo> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/<controller>/5
+        public StationInfo Get(int id)
+        {
+            var stationInfo = busTracker.GetStationInfo(id);
+            return stationInfo;
+        }
+    }
+}
